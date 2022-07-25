27c37a42e47f7f54a6862c949d6dfaf8649c128c
macs0021 <macs0021@red.ujaen.es>
2022-07-25 19:11:15 +0200

    reproduction and sending basic implementation

--- Assets/Scripts/FamilyController.cs
+++ Assets/Scripts/FamilyController.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class FamilyController : MonoBehaviour
 {
     [SerializeField] IndividualController individualController;
-    
+
     private ParcelController _parcelController;
-    
+
     private List<IndividualController> _individuals = new List<IndividualController>();
 
     public void Init(ParcelController parcelController)
@@ -19,24 +19,72 @@ public class FamilyController : MonoBehaviour
     {
         int familySize = _individuals.Count;
 
-        if (familySize < 6)
+        if (familySize < 6 && familySize > 1)
         {
             CreateIndividual();
         }
+        else if (familySize == 6)
+        {
+            List<ParcelController> neighbours = _parcelController.GetNeighbours();
+            int index = Random.Range(0, neighbours.Count);
+
+            //searching for a random non full neighbour
+            while (neighbours[index].GetComponent<FamilyController>().isFull() || neighbours.Count == 0)
+            {
+                neighbours.Remove(neighbours[index]);
+                index = Random.Range(0, neighbours.Count);
+            }
+            //sending an individual to the selected tile
+            IndividualController i = Instantiate(individualController, _parcelController.transform.position, Quaternion.identity);
+            neighbours[index].GetComponent<FamilyController>().AssignNewIndividual(i);
+
+        }
         else
         {
-            
+
         }
     }
-    
+    public void Reproduce()
+    {
+        int familySize = _individuals.Count;
+
+        if (familySize < 6 && familySize > 1)
+        {
+            CreateIndividual();
+        }
+    }
+    public void Send()
+    {
+        int familySize = _individuals.Count;
+
+        if (familySize == 6)
+        {
+            List<ParcelController> neighbours = _parcelController.GetNeighbours();
+            int index = Random.Range(0, neighbours.Count);
+
+            //searching for a random non full neighbour
+            while (neighbours.Count > 0 && neighbours[index].GetComponent<FamilyController>().isFull())
+            {
+                neighbours.Remove(neighbours[index]);
+                index = Random.Range(0, neighbours.Count);
+                Debug.Log(index.ToString());
+            }
+            if (neighbours.Count != 0)
+            {
+                //sending an individual to the selected tile
+                IndividualController i = Instantiate(individualController, _parcelController.transform.position, Quaternion.identity);
+                neighbours[index].GetComponent<FamilyController>().AssignNewIndividual(i);
+            }
+        }
+    }
     public void CreateIndividual()
     {
         IndividualController i = Instantiate(individualController, _parcelController.transform.position, Quaternion.identity);
-        
+
         _individuals.Add(i);
-        
+
         Vector3 newbornFormation = DoFormationNewborn();
-        
+
         i.Birth(() =>
         {
             i.MoveTowards(newbornFormation);
@@ -51,10 +99,10 @@ public class FamilyController : MonoBehaviour
         {
             _individuals[i].MoveTowards(formationPositions[i]);
         }
-        
+
         _individuals[^1].MoveTowardsNewFamily(_parcelController.transform.position, formationPositions[^1]);
     }
-    
+
     private Vector3 DoFormationNewborn()
     {
         List<Vector3> formationPositions = _parcelController.GetFormationPositions(_individuals.Count);
@@ -63,14 +111,19 @@ public class FamilyController : MonoBehaviour
         {
             _individuals[i].MoveTowards(formationPositions[i]);
         }
-        
+
         return formationPositions[^1];
     }
 
     public void AssignNewIndividual(IndividualController individual)
     {
         _individuals.Add(individual);
-        
         DoFormation();
+    }
+    public bool isFull()
+    {
+        if (_individuals.Count >= 6)
+            return true;
+        return false;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private WorldGrid _worldGrid;
-    
+    FamilyController startFamily;
+
     // Timer
     public float timeLeft = 5.0f;
     private float timer = 0.0f;
 
-    // Update is called once per frame
+    public float reproduceTime;
+    public float sendingTime;
+    float _lastReproduction;
+    float _lastSendingTime;
+
+    private void Start()
+    {
+
+    }
     void Update()
     {
         // Spawn and individual when timer
-        
+
         /*if (timer >= timeLeft)
         {
             // Spawn individual
@@ -28,9 +37,28 @@ public class GameManager : MonoBehaviour
             timer += Time.deltaTime;
         }*/
 
+
+        if (Time.time > reproduceTime + _lastReproduction)
+        {
+            DoStep();
+            _lastReproduction = Time.time;
+            Invoke("DoStep2", 1f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (!startFamily)
+                GetStartingFamily();
+            RandomSpawn();
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             DoStep();
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            DoStep2();
         }
     }
 
@@ -38,7 +66,26 @@ public class GameManager : MonoBehaviour
     {
         foreach (FamilyController family in _worldGrid.GetFamilies())
         {
-            family.DoStep();
+            family.Reproduce();
         }
+
+    }
+    public void DoStep2()
+    {
+        foreach (FamilyController family in _worldGrid.GetFamilies())
+        {
+            family.Send();
+        }
+    }
+
+    //test spawning function
+    void RandomSpawn()
+    {
+        startFamily.CreateIndividual();
+
+    }
+    void GetStartingFamily()
+    {
+        startFamily = _worldGrid.GetRandomParcel().GetComponent<FamilyController>();
     }
 }
